02837d45a85c248e7b1c5d0d65f2d3851e3c9fde
Чернов Евгений Игоревич <eugene.chernov@synchro.pro>
2024-06-27 15:32:41 +0300

    fix

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 @app.get('/')
 def read_root():
-    return {"Welcome": "Hi"}
+    return {"Welcome": "asshole"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
